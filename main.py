0aca7d6d47449f011fc07069e09d9451fa90590b
UnderachieversDK <44040829+UnderachieversDK@users.noreply.github.com>
2023-05-19 15:54:46 -0400

    Update main.py
    Starting the groundwork for JSON logging of votes

--- main.py
+++ main.py
@@ -15,8 +15,17 @@ bot.remove_command("help")
 default_color = discord.Color(0xfa8072)
 admins = [206636330095083523]
 
-@bot.event
-async def on_ready():
-	
+
+def addVote(userid, employee, month, year):
+    file_path = f'resources/Votes/{month}.{year}.json'
+    if os.path.isfile(file_path) == True:
+    	file = open(filepath)
+    	file_data = json.load(file)
+        
+def createVote(employee1, employee2, employee3, month, year):
+    file_path = f'resources/Votes/{month}.{year}.json'
+    json_object = {"employees": [employee1, employee2, employee3], employee1: [], employee2: [], employee3: []}}
+    with open(file_path, "w+") as file:
+		file.write(json_object)
 
 bot.run('')
